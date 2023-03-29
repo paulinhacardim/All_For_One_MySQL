e58b132844cbfa461e8774d7c429a6619bdd995b
paulinha <paulinha@MacBook-Pro-de-seguranca.local>
2023-03-29 11:33:07 -0300

    Requisito 23

---
+++ desafio23.sql
@@ -0,0 +1,8 @@
+UPDATE order_details
+SET discount = 30
+WHERE unit_price <  10.0000;
+
+
+-- UPDATE = nome da tabela
+-- SET = nome da coluna, propreeidade a ser alterada
+-- WHERE = onde, 'condição'
